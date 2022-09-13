1095c6d2cfbb50f68475ecfa3d8a74f5922d13ac
7jjin <wlsgud6305@naver.com>
2022-09-13 21:20:01 +0900

    JS: arrow-up

--- main.js
+++ main.js
@@ -35,7 +35,20 @@ document.addEventListener('scroll',()=>{
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// Show "arrow-up" button when scrolling done
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=>{
+    if(window.scrollY > homeHeight /2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+});
 
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click',()=>{
+    scrollIntoView('.home');
+})
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
